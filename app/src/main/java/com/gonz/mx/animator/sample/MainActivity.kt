a774b081490cfd96b8ea1a2b0e330174ea84d8da
Gonzalo Campos <erick.campos@wizeline.com>
2020-01-07 15:48:19 -0600

    Creates object animator in XMl resource

--- app/src/main/java/com/gonz/mx/animator/sample/MainActivity.kt
+++ app/src/main/java/com/gonz/mx/animator/sample/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.gonz.mx.animator.sample
 
+import android.animation.AnimatorInflater
 import android.animation.ObjectAnimator
 import android.animation.PropertyValuesHolder
 import androidx.appcompat.app.AppCompatActivity
@@ -15,13 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        vwAnimatorTest.setOnClickListener {
-            animateWithAnimatorApi(it)
+        btnCodeAnimator.setOnClickListener {
+            animateWithAnimatorApiUsingCode(vwAnimatorTest)
+        }
+
+        btnXmlAnimator.setOnClickListener {
+            animateWithAnimatorApiUsingXML(vwAnimatorTest)
         }
     }
 
-    private fun animateWithAnimatorApi(view: View) {
-
+    private fun animateWithAnimatorApiUsingCode(view: View) {
+        // This code creates the animation from scratch
         val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0.5f, 1f)
         val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.5f, 1f)
         val alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 0f, 1f)
@@ -29,5 +34,16 @@ class MainActivity : AppCompatActivity() {
         ObjectAnimator.ofPropertyValuesHolder(view, scaleX, scaleY, alpha)
             .apply { interpolator = OvershootInterpolator() }
             .start()
+    }
+
+
+    private fun animateWithAnimatorApiUsingXML(view: View) {
+        // This code takes the animation from an XML resource, an inflates
+        // The result of the animation is exactly the same
+        AnimatorInflater.loadAnimator(this, R.animator.obejct_animator_example)
+            .apply {
+                setTarget(view)
+                start()
+            }
     }
 }
